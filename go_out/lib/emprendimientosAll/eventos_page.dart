89dbc160e96d98466bfa43b48cbb384ddbe9aa3d
DavidAlex99 <daalrive@espol.edu.ec>
2024-03-12 01:54:45 -0500

    funcionalidad consumo redireccion a emprendimiento respectivo tras elegir evento

--- go_out/lib/emprendimientosAll/eventos_page.dart
+++ go_out/lib/emprendimientosAll/eventos_page.dart
@@ -2,6 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import './emprendimiento_detalles_main.dart';
+
+Future<Map> fetchEmprendimientoDetails(int emprendimientoId) async {
+  final String url =
+      'http://192.168.100.6:8000/goOutApp/emprendimientos/$emprendimientoId';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('Failed to load emprendimiento details');
+  }
+}
 
 class EventosPage extends StatefulWidget {
   @override
@@ -37,38 +50,54 @@ class _EventosPageState extends State<EventosPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Eventos'),
-        actions: <Widget>[
-          DropdownButton<String>(
-            value: selectedCategory,
-            onChanged: (String? newValue) {
-              setState(() {
-                selectedCategory = newValue;
-                fetchEventos();
-              });
-            },
-            items: <String>['Todos', 'ARTISTICAS', 'CULTURALES', 'DEPORTIVO']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: eventos.length,
-        itemBuilder: (context, index) {
-          var evento = eventos[index];
-          return ListTile(
-            title: Text(evento['titulo']),
-            subtitle: Text('${evento['descripcion']} - \$${evento['precio']}'),
-            // Añade más detalles según sea necesario
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Eventos'),
+          actions: <Widget>[
+            DropdownButton<String>(
+              value: selectedCategory,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedCategory = newValue;
+                  fetchEventos();
+                });
+              },
+              items: <String>['Todos', 'ARTISTICAS', 'CULTURALES', 'DEPORTIVO']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: eventos.length,
+          itemBuilder: (context, index) {
+            var evento = eventos[index];
+            return ListTile(
+              title: Text(evento['titulo']),
+              subtitle:
+                  Text('${evento['descripcion']} - \$${evento['precio']}'),
+              onTap: () async {
+                try {
+                  var emprendimientoDetails = await fetchEmprendimientoDetails(
+                      evento['emprendimiento_id']);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EmprendimientoDetallesPage(
+                          emprendimiento: emprendimientoDetails),
+                    ),
+                  );
+                } catch (e) {
+                  // Manejar el error, por ejemplo, mostrando un mensaje al usuario
+                  print(
+                      e); // Considera usar un enfoque más robusto para manejar y reportar errores
+                }
+              },
+            );
+          },
+        ));
   }
 }
