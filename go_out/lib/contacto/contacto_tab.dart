08c645df8053f871829161d92bb1264912905f8c
DavidAlex99 <daalrive@espol.edu.ec>
2024-03-21 12:33:50 -0500

    funcionalidad de geolocalizacion del cliente con respecto a la ubicacion del emprendimiento

--- go_out/lib/contacto/contacto_tab.dart
+++ go_out/lib/contacto/contacto_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:math';
 
 class ContactoTab extends StatefulWidget {
   final Map emprendimiento;
@@ -12,68 +15,124 @@ class ContactoTab extends StatefulWidget {
 
 class _ContactoTabState extends State<ContactoTab> {
   GoogleMapController? mapController;
+  // para arcar la ubicacion del cliente
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializar el marcador del emprendimiento desde el inicio.
+    final latitud =
+        double.tryParse('${widget.emprendimiento['contacto']['latitud']}');
+    final longitud =
+        double.tryParse('${widget.emprendimiento['contacto']['longitud']}');
+    if (latitud != null && longitud != null) {
+      markers.add(Marker(
+        markerId: MarkerId("emprendimientoLocation"),
+        position: LatLng(latitud, longitud),
+      ));
+    }
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    print(
-        "Contacto en contactoDetalle: ${widget.emprendimiento['contacto'].toString()}");
-    final contacto = widget.emprendimiento['contacto'] ?? {};
-    final imagenesContacto = contacto['imagenesContacto'] ?? [];
-
-    final latitud = double.tryParse('${contacto['latitud']}');
-    final longitud = double.tryParse('${contacto['longitud']}');
-
-    if (latitud == null || longitud == null) {
-      print("Error: Las coordenadas no son válidas.");
-      return Center(child: Text("Ubicación del emprendimiento no disponible."));
-    } else {
-      print(
-          "Coordenadas del emprendimiento: Latitud: $latitud, Longitud: $longitud");
+  // obtener permiso ubicacion del cliente
+  Future<void> _getUserLocation() async {
+    // Verifica y solicita los permisos de ubicación.
+    var status = await Permission.locationWhenInUse.status;
+    if (status.isDenied) {
+      // Los permisos están denegados, solicítalos.
+      status = await Permission.locationWhenInUse.request();
+      if (status.isDenied) {
+        // Los permisos fueron denegados definitivamente.
+        print('Permiso de ubicación denegado');
+        return;
+      }
     }
 
-    final LatLng emprendimientoLocation = LatLng(latitud, longitud);
+    if (status.isPermanentlyDenied) {
+      // Los permisos están denegados permanentemente, dirige al usuario a la configuración.
+      openAppSettings();
+      return;
+    }
 
+    // Asumiendo que ya has añadido el marcador del emprendimiento y del usuario a 'markers'
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      markers.add(Marker(
+        markerId: MarkerId('userLocation'),
+        position: LatLng(position.latitude, position.longitude),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      ));
+    });
+
+    // Ubicación del emprendimiento.
+    final LatLng emprendimientoLocation = LatLng(
+        double.tryParse('${widget.emprendimiento['contacto']['latitud']}') ?? 0,
+        double.tryParse('${widget.emprendimiento['contacto']['longitud']}') ??
+            0);
+
+    // Crear LatLngBounds
+    final LatLngBounds bounds = LatLngBounds(
+      southwest: LatLng(
+        min(emprendimientoLocation.latitude, position.latitude),
+        min(emprendimientoLocation.longitude, position.longitude),
+      ),
+      northeast: LatLng(
+        max(emprendimientoLocation.latitude, position.latitude),
+        max(emprendimientoLocation.longitude, position.longitude),
+      ),
+    );
+
+    // Ajustar la cámara para mostrar ambos marcadores
+    mapController?.animateCamera(CameraUpdate.newLatLngBounds(bounds, 100));
+  }
+  // fin obtener permiso ubicacion del cliente
+
+  @override
+  Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          if (latitud != null && longitud != null)
-            Container(
-              height: 300.0,
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: emprendimientoLocation,
-                  zoom: 16.0,
-                ),
-                markers: {
-                  Marker(
-                    markerId: MarkerId('emprendimientoMarker'),
-                    position: emprendimientoLocation,
-                  ),
-                },
+          Container(
+            height: 300.0,
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(widget.emprendimiento['contacto']['latitud'],
+                    widget.emprendimiento['contacto']['longitud']),
+                zoom: 16.0,
               ),
+              markers: markers,
             ),
+          ),
+          ElevatedButton(
+            onPressed: _getUserLocation,
+            child: Text('Mostrar mi ubicación'),
+          ),
+          // Las siguientes ListTiles son iguales que en tu código anterior.
           ListTile(
             title: Text('Dirección:'),
-            subtitle: Text(contacto['direccion'] ?? 'No disponible'),
+            subtitle: Text(widget.emprendimiento['contacto']['direccion'] ??
+                'No disponible'),
             leading: Icon(Icons.location_on),
           ),
           ListTile(
             title: Text('Teléfono:'),
-            subtitle: Text(contacto['telefono'] ?? 'No disponible'),
+            subtitle: Text(widget.emprendimiento['contacto']['telefono'] ??
+                'No disponible'),
             leading: Icon(Icons.phone),
           ),
           ListTile(
             title: Text('Correo Electrónico:'),
-            subtitle: Text(contacto['correo'] ?? 'No disponible'),
+            subtitle: Text(
+                widget.emprendimiento['contacto']['correo'] ?? 'No disponible'),
             leading: Icon(Icons.email),
           ),
-          // Aquí mostramos las imágenes sin usar CarouselSlider
-          for (var img in imagenesContacto)
+          for (var img
+              in widget.emprendimiento['contacto']['imagenesContacto'] ?? [])
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Image.network(
